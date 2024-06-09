f5a185555febb92c8f7c83bd032431fbe8cc8f4a
Akshat Agarwal <hey@akshat.dev>
2024-06-10 01:55:44 +0530

    feat: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,51 @@
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my portfolio</h1>
-      <p className="prose prose-neutral dark:prose-invert">
-        This is your new portfolio.
-      </p>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">
+        hey, I'm akshat 👋
+      </h1>
+      <p className="prose prose-neutral dark:prose-invert"></p>
+      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="mailto:hey@akshat.dev"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">get in touch</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.linkedin.com/in/humancalico/"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">connect on linkedin</p>
+          </a>
+        </li>
+      </ul>
     </section>
+  );
+}
+
+function ArrowIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+        fill="currentColor"
+      />
+    </svg>
   );
 }
