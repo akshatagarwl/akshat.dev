06a7b7467aa63fdd5804e2662a399c315f322634
Akshat Agarwal <hey@akshat.dev>
2024-06-10 02:00:55 +0530

    chore: temporarily remove blog page

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,15 +1,8 @@
-import { getBlogPosts } from 'app/db/blog';
-
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
-    url: `https://akshat.dev/blog/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }));
-
-  let routes = ['', '/blog', '/work'].map((route) => ({
+  let routes = ['', '/work'].map((route) => ({
     url: `https://akshat.dev${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }));
 
-  return [...routes, ...blogs];
+  return [...routes];
 }
